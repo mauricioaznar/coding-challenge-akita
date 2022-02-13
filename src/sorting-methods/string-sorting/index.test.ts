af6ae8988badbaff29d920f00d252714bce38bc2
mauricioaznar <mauricioaznar94@gmail.com>
2022-02-13 10:56:59 -0600

    poker

--- src/sorting-methods/string-sorting/index.test.ts
+++ src/sorting-methods/string-sorting/index.test.ts
@@ -1,6 +1,11 @@
 import { stringSort } from './index';
 
-it('returns string array sorted', () => {
+it('returns string array sorted 1', () => {
   const strings = ['dog', 'cat', 'cow'];
   expect(stringSort(strings)).toEqual(['cat', 'cow', 'dog']);
+});
+
+it('returns string array sorted 2', () => {
+  const strings = ['caaar', 'car', 'Car'];
+  expect(stringSort(strings)).toEqual(['Car', 'caaar', 'car']);
 });
--- src/sorting-methods/string-sorting/index.ts
+++ src/sorting-methods/string-sorting/index.ts
@@ -1,3 +1,31 @@
+import { isALessThanB } from './functions/is-a-less-than-b';
+
 export function stringSort(strings: string[]): string[] {
-  return strings.sort();
+  const sortedStrings: string[] = [];
+
+  for (let i = 0; i < strings.length; i++) {
+    const currString = strings[i];
+    let isPositionFound = false;
+
+    let j = 0;
+    while (isPositionFound === false && j < sortedStrings.length) {
+      const currSortedString = sortedStrings[j];
+      const isLess = isALessThanB(currString, currSortedString);
+      if (isLess) {
+        isPositionFound = true;
+      } else {
+        j++;
+      }
+    }
+
+    sortedStrings.splice(j, 0, currString);
+
+    // if (isPositionFound === false) {
+    //   sortedStrings.push(currString);
+    // } else {
+    //   sortedStrings.splice(j, 0, currString);
+    // }
+  }
+
+  return sortedStrings;
 }
