feca858b18fcff6d66974a3b0722eb7089f4e110
mauricioaznar <mauricioaznar94@gmail.com>
2022-03-02 15:33:22 -0600

    initial commit

--- src/sorting-methods/string-sorting/index.ts
+++ src/sorting-methods/string-sorting/index.ts
@@ -1,3 +1,45 @@
-export function stringSort(strings: string[]): string[] {
-  return strings.sort();
+export function stringSort(array: string[]): string[] {
+
+  for(let i = 0; i < array.length - 1; i++){
+    const splitedI = array[i].split('');
+
+    for(let j= i + 1; j < array.length; j++){
+      const splitedJ = array[j].split('');
+
+      if(splitedI > splitedJ){
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+  }
+
+  return array;
 }
+
+
+/*
+  strings.forEach(item => {
+    let inserted = false
+    for (let i = 0; i < arr.length; i++) {
+
+
+
+      const currArrItem = arr[i]
+      if (item < currArrItem && currArrItem !== item) {
+        inserted = true
+        arr.unshift(currArrItem)
+        break
+      }
+
+
+    }
+    console.log(item)
+
+    if (inserted === false) {
+      arr.push(item)
+    }
+
+  })
+
+
+  console.log(arr)
+ */
